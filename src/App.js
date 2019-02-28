e3b8e0eb49b3f739ba58b8c67c963565c104682d
CalfSlicer <CalfSlicer@gmail.com>
2019-02-28 10:53:40 -0800

    change to app.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        WOOT! REACT IN DOCKER CONTAINER!
+        HELLO FUCKIN WORLD
       </div>
     );
   }
